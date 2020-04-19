78669d5cd6563844487249ae7b37aff537b5b4f9
JCDraaijer <jd222pg@student.lnu.se>
2020-04-19 20:02:28 +0200

    Add license and readme

---
+++ README.md
@@ -0,0 +1,11 @@
+#MCpp
+
+An implementation of the Minecraft server in C++
+
+#Dependencies
+To build this project, `cmake`, version 3.,14 or higher is required.
+
+MCpp requires `pthread`.
+
+#License
+This software is licensed under GPLv2. See `LICENSE.md` for more information.
--- protocol/PacketParser.cpp
+++ protocol/PacketParser.cpp
@@ -1,6 +1,21 @@
-//
-// Created by jona on 2019-06-10.
-//
+/*
+MCpp, a Minecraft Server implementation in C++.
+Copyright (C) 2019, 2020 Johannes Draaijer
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 
 #include <unistd.h>
 #include <strings.h>
@@ -8,7 +23,6 @@
 #include "in/PacketInHandshake.hpp"
 #include "in/PacketInStatusRequest.hpp"
 #include "in/PacketInLoginStart.hpp"
-#include "in/PacketInLoginEncryptionResponse.hpp"
 #include "in/PacketInStatusPing.hpp"
 #include "in/PacketInPlayPluginMessage.hpp"
 #include "Identifier.hpp"
